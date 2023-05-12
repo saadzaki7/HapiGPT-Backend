6d54879cc934fb5a370acfb32e2fe0341f28f661
Saad Zaki <65866779+saadzaki7@users.noreply.github.com>
2023-05-12 12:08:52 -0400

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-app.post("/find-complexity", async (req, res) => {
+app.post("/find-response", async (req, res) => {
   try {
     const { prompt } = req.body;
     curMsg=[{role: "system", content: "A curious therapist that asks questions"}]
